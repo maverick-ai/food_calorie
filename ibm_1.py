b58fd0dad4ff255bed7f37f9cc6ef17e6542d412
Sarthak Bansal <59017670+maverick-ai@users.noreply.github.com>
2020-06-13 19:06:48 +0530

    Update ibm_1.py

--- ibm_1.py
+++ ibm_1.py
@@ -3,13 +3,13 @@ import json
 from ibm_watson import VisualRecognitionV3
 from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
 
-authenticator = IAMAuthenticator('QeULZ3tNyXhuGlh4eQWn0DTJgcQET86_fym6s3Yn_A8z')
+authenticator = IAMAuthenticator('API key')
 visual_recognition = VisualRecognitionV3(
     version='2018-03-19',
     authenticator=authenticator
 )
 
-visual_recognition.set_service_url('https://api.us-south.visual-recognition.watson.cloud.ibm.com/instances/e83c2dab-5735-4074-8358-f792e6c3dc47')
+visual_recognition.set_service_url('api url')
 
 
 with open('/Users/sushmabansal/Desktop/food_data/images/apple_pie/134.jpg', 'rb') as images_file:
