272d9beddb9e44a24172faf988f709a4f5d4812e
Sarthak Bansal <59017670+maverick-ai@users.noreply.github.com>
2020-06-13 19:06:08 +0530

    Update ibm.py

--- ibm.py
+++ ibm.py
@@ -2,15 +2,15 @@ import json
 from ibm_watson import VisualRecognitionV3
 from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
 
-authenticator = IAMAuthenticator('QeULZ3tNyXhuGlh4eQWn0DTJgcQET86_fym6s3Yn_A8z')
+authenticator = IAMAuthenticator('API key')
 visual_recognition = VisualRecognitionV3(
     version='2018-03-19',
     authenticator=authenticator
 )
 
-visual_recognition.set_service_url('https://api.us-south.visual-recognition.watson.cloud.ibm.com/instances/e83c2dab-5735-4074-8358-f792e6c3dc47')
+visual_recognition.set_service_url('api url')
 
-url = 'https://www.biggerbolderbaking.com/wp-content/uploads/2019/07/15-Minute-Pizza-WS-Thumbnail.png'
+url = 'image url'
 classifier_ids = ["food"]
 
 classes_result = visual_recognition.classify(url=url, classifier_ids=classifier_ids).get_result()
